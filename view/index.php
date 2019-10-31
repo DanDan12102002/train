29a348c97647e6d95f10ae8e1546d529d4a0e6e2
Dan Pro <prozardan@gmail.com>
2019-10-31 19:12:19 +0200

    Done for today

--- view/index.php
+++ view/index.php
@@ -5,6 +5,25 @@
 	</head>
 	<body>
 		<div class="wrapper">
+			<header>
+				<div class="container">
+					<div class="row">
+						<div class="col-3">
+							<div class="logo">
+								<a href="/"><img src="../assets/images/logo.png" alt=""></a>
+							</div>
+						</div>
+						<div class="col-8">
+							<nav class="menu">
+								<li class="go-to-block" data-target=".banner">Home</li>
+								<li class="go-to-block" data-target=".about">About us</li>
+								<li class="go-to-block" data-target=".how-works">How it works</li>
+								<li class="go-to-block" data-target=".contact">Contact us</li>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</header>
 			<section class="banner">
 				<div class="container">
 					
